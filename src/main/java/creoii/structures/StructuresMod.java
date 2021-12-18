7bdcf32060c5a50d73d01bbb27ab5062ba1055ab
creoii <33011337+creoii@users.noreply.github.com>
2021-12-18 02:35:58 -0500

    Update StructuresMod.java

--- src/main/java/creoii/structures/StructuresMod.java
+++ src/main/java/creoii/structures/StructuresMod.java
@@ -7,8 +7,11 @@ import net.fabricmc.api.ModInitializer;
 public class StructuresMod implements ModInitializer {
     public static final String MOD_ID = "structures";
 
-    //TODO: Underground Shacks, Pillager Castles, Terracotta Tombs, Larger Monster Dungeons, Big Beehives, Halls
-
+    //TODO: Underground Shacks, Pillager Castles, Terracotta Tombs, Larger Monster Dungeons, Big Beehives, Halls, Totems, Easter Island Heads
+    // Shacks should be biome dependent: Stone Peaks, Snowy Peaks, etc
+    // Jungle Pyramids should have Emerald Blocks
+    // Halls are biome dependent: Ice, Normal, Desert, Badlands, Dark Oak
+    
     @Override
     public void onInitialize() {
         StructureRegistry.register();
